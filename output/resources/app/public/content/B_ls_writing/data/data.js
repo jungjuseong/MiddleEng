35d5f8cdb0dc772e490e17838afc8079622a1b10
seongmin.jung <jsm9657@gmail.com>
2020-11-06 21:39:43 +0900

    b_ls_writing 에 ls_comprehesion 전부 복사

--- output/resources/app/public/content/B_ls_writing/data/data.js
+++ output/resources/app/public/content/B_ls_writing/data/data.js
@@ -1,31 +1,628 @@
-var _data = {
-	page1: {
-		quizs: [
-			{seq: 1,       question: "가방 샀어요? 정말 {예쁘네요.}",			choice1: "예쁘네요.",							choice2: "덥네요.",						correct: 1},
-			{seq: 2,       question: "밖에 비가 {오네요.}",						choice1: "흐리네요.",							choice2: "오네요.",						correct: 2},
-			{seq: 3,       question: "승윤 씨, {우산 있어요?}",					choice1: "네, 있어요.",							choice2: "우산 있어요?",					correct: 2},
-			{seq: 4,       question: "벌써 10시{네요.}",						choice1: "이다.",								choice2: "네요.",							correct: 2},
-			{seq: 5,       question: "백화점에서 이 가방{이랑} 바지를 샀어요.",	choice1: "랑",								choice2: "이랑",							correct: 2},
-			{seq: 6,       question: "{마트에서} 사과랑 딸기를 샀어요.",			choice1: "마트서",							choice2: "마트에서",						correct: 2},
-			{seq: 7,       question: "가방에 뭔가 있어요?",						choice1: "정말 멋있네요",						choice2: "책이랑 공책이랑 있어요.",			correct: 2},
-			{seq: 8,       question: "얀토 씨는 지금 {청소하네요.}",				choice1: "청소해네요.",						choice2: "청소하네요.",					correct: 2},
-			{seq: 9,       question: "이 가수 노래가 어때요?",					choice1: "정말 좋네요.",						choice2: "어제 유라랑 놀았어요.",			correct: 1},
-			{seq: 10,     question: "빵{이랑} 우유를 먹었어요.",				choice1: "이랑",								choice2: "랑",							correct: 1},
-			{seq: 11,     question: "민호 씨는 밥을 많이 {먹네요.}",				choice1: "먹네요.",							choice2: "먹으네요.",						correct: 1},
-			{seq: 12,     question: "{저랑} 같이 가요.",						choice1: "저는",								choice2: "저랑",							correct: 2},
-			{seq: 13,     question: "에릭 씨, 여기가 명동이에요.",				choice1: "네, 하늘도 정말 파랗네요.",			choice2: "와, 사람이 많네요.",				correct: 2},
-			{seq: 14,     question: "점심에는 김밥{이랑} 떡볶이를 먹을까요?",	choice1: "랑",								choice2: "이랑",							correct: 2},
-			{seq: 15,     question: "승윤 씨는 누구랑 살아요?",				choice1: "저랑 이 학교에 다녀요.",				choice2: "동생이랑 살아요.",				correct: 2},
-			{seq: 16,     question: "어제 누구랑 놀았어요?",					choice1: "어제 윤오랑 학교에 갔어요.",			choice2: "어제 윤오랑 놀았어요.",			correct: 2},
-			{seq: 17,     question: "떡볶이 맛이 어때요?",						choice1: "떡볶이가 많이 맵네요.",				choice2: "떡볶이를 맛있게 했네요.",			correct: 1},
-			{seq: 18,     question: "오늘 저녁에는 뭘 할 거예요?",				choice1: "공원에 나무가 많네요.",				choice2: "영화를 보고 밥을 먹을 거예요.",	correct: 2},
-			{seq: 19,     question: "여름에는 날씨가 어때요?",					choice1: "여름에는 비가 많이 오고 더워요.",		choice2: "여름에는 휴가를 갈 거예요.",		correct: 1},
-			{seq: 20,     question: "겨울에는 날씨가 어때요?",					choice1: "겨울에는 스키를 타러 갈 거예요.",		choice2: "겨울에는 눈이 오고 추워요.",		correct: 2}
-		]
-	}
+﻿var _data = {
+    "video": "y3_l1_u1_011_main.mp4",
+    "video_start": 0,
+    "speakerA": {
+        "name": "Sam",
+        "image_s": "sam_small.png",
+        "image_l": "sam.png"
+    },
+    "speakerB": {
+        "name": "Anna",
+        "image_s": "anna_small.png",
+        "image_l": "anna.png"
+    },
+    "speakerC": {
+        "name": "Maria",
+        "image_s": "maria_small.png",
+        "image_l": "maria.png"
+    },
+    "speakerD": {
+        "name": "Ms. Jones",
+        "image_s": "ms. jones_small.png",
+        "image_l": "ms. jones.png"
+    },
+    "speakerE": {
+        "name": "Sam&Anna",
+        "image_s": "sam&anna_small.png",
+        "image_l": "sam&anna.png"
+    },
+    "letstalk": {
+        "sentence": "How do you say \"hi\" in different languages?",
+        "audio": "y3l1u1_011_letstalk_01.mp3",
+        "img1": "y3l1u1_011_letstalk_01.jpg",
+        "img2": "",
+        "sample": "We say \"{Hola}\" in {Spanish}.",
+        "hint": "We say \"Hola\" in Spanish."
+    },
+    "quizs": [{
+        "seq": 1,
+        "SC_DIV1": "C0302",
+        "SC_DIV2": "",
+        "SC_DIV3": "",
+        "SC_DIV4": "",
+        "SC_SAVE": true,
+        "question": "Where is Maria from?",
+        "audio": "y3l1u1n1_lc_q_001.mp3",
+        "answer": 1,
+        "choice_1": "Spain",
+        "choice_2": "Hawaii",
+        "choice_3": "Mexico",
+        "tmq_seq": 9783,
+        "tmq_CDC": ["C0302"],
+        "tmq_COD": []
+    }, {
+        "seq": 2,
+        "SC_DIV1": "C0302",
+        "SC_DIV2": "",
+        "SC_DIV3": "",
+        "SC_DIV4": "",
+        "SC_SAVE": true,
+        "question": "Where is Anna from?",
+        "audio": "y3l1u1n1_lc_q_002.mp3",
+        "answer": 2,
+        "choice_1": "Spain",
+        "choice_2": "Hawaii",
+        "choice_3": "Mexico",
+        "tmq_seq": 9784,
+        "tmq_CDC": ["C0302"],
+        "tmq_COD": []
+    }, {
+        "seq": 3,
+        "SC_DIV1": "C0303",
+        "SC_DIV2": "",
+        "SC_DIV3": "",
+        "SC_DIV4": "",
+        "SC_SAVE": true,
+        "question": "What will Sam and Anna do next?",
+        "audio": "y3l1u1n1_lc_q_003.mp3",
+        "answer": 3,
+        "choice_1": "have lunch with Maria",
+        "choice_2": "talk with Maria in Spanish",
+        "choice_3": "show Maria around the school",
+        "tmq_seq": 9785,
+        "tmq_CDC": ["C0303"],
+        "tmq_COD": []
+    }],
+    "scripts": [{
+        "seq": 1,
+        "dms_seq": 117256,
+        "dms_speaker": "Ms. Jones",
+        "dms_start": 22.25,
+        "dms_end": 23,
+        "qnums": [],
+        "dms_eng": "Hello!",
+        "dms_kor": {
+            "ko": "안녕",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 2,
+        "dms_seq": 117257,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117257"
+        }],
+        "dms_speaker": "Ms. Jones",
+        "dms_start": 23.15,
+        "dms_end": 24.39,
+        "qnums": [],
+        "dms_eng": "You must be Maria!",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 3,
+        "dms_seq": 117258,
+        "dms_speaker": "Maria",
+        "dms_start": 24.75,
+        "dms_end": 25.62,
+        "qnums": [],
+        "dms_eng": "Hello!",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 4,
+        "dms_seq": 117259,
+        "dms_speaker": "Ms. Jones",
+        "dms_start": 25.64,
+        "dms_end": 26.98,
+        "qnums": [],
+        "dms_eng": "I'm Ms. Jones.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 5,
+        "dms_seq": 117260,
+        "dms_speaker": "Ms. Jones",
+        "dms_start": 27.11,
+        "dms_end": 27.89,
+        "qnums": [],
+        "dms_eng": "Come on in.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 6,
+        "dms_seq": 117261,
+        "dms_speaker": "Ms. Jones",
+        "dms_start": 31.9,
+        "dms_end": 33.19,
+        "qnums": [],
+        "dms_eng": "Sam! Anna!",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 7,
+        "dms_seq": 117262,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117262"
+        }],
+        "dms_speaker": "Ms. Jones",
+        "dms_start": 33.23,
+        "dms_end": 34.88,
+        "qnums": [],
+        "dms_eng": "Could you come here, please?",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 8,
+        "dms_seq": 117263,
+        "dms_speaker": "Ms. Jones",
+        "dms_start": 40.63,
+        "dms_end": 41.82,
+        "qnums": [],
+        "dms_eng": "This is Maria.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 9,
+        "dms_seq": 117264,
+        "dms_speaker": "Ms. Jones",
+        "dms_start": 42.05,
+        "dms_end": 43.58,
+        "qnums": [],
+        "dms_eng": "She is new to our school.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 10,
+        "dms_seq": 117265,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117265"
+        }],
+        "dms_speaker": "Ms. Jones",
+        "dms_start": 43.7,
+        "dms_end": 45.61,
+        "qnums": [],
+        "dms_eng": "She is from another country.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 11,
+        "dms_seq": 117266,
+        "dms_speaker": "Sam",
+        "dms_start": 46.24,
+        "dms_end": 48.15,
+        "qnums": [],
+        "dms_eng": "Hi, I'm Sam.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 12,
+        "dms_seq": 117267,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117267"
+        }],
+        "dms_speaker": "Sam",
+        "dms_start": 48.36,
+        "dms_end": 49.53,
+        "qnums": [],
+        "dms_eng": "Where are you from?",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 13,
+        "dms_seq": 117268,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117268"
+        }],
+        "dms_speaker": "Maria",
+        "dms_start": 49.83,
+        "dms_end": 51.72,
+        "qnums": [1],
+        "dms_eng": "Hi, I'm from Spain.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 14,
+        "dms_seq": 117269,
+        "dms_speaker": "Sam",
+        "dms_start": 51.8,
+        "dms_end": 52.88,
+        "qnums": [],
+        "dms_eng": "Wow!",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 15,
+        "dms_seq": 117270,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117270"
+        }],
+        "dms_speaker": "Sam",
+        "dms_start": 52.94,
+        "dms_end": 54.92,
+        "qnums": [],
+        "dms_eng": "What language do you speak?",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 16,
+        "dms_seq": 117271,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117271"
+        }],
+        "dms_speaker": "Maria",
+        "dms_start": 55.03,
+        "dms_end": 56.6,
+        "qnums": [],
+        "dms_eng": "I speak Spanish.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 17,
+        "dms_seq": 117272,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117272"
+        }],
+        "dms_speaker": "Sam",
+        "dms_start": 56.81,
+        "dms_end": 60.15,
+        "qnums": [],
+        "dms_eng": "How do you say \"hi\" in Spanish?",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 18,
+        "dms_seq": 117273,
+        "dms_speaker": "Maria",
+        "dms_start": 60.16,
+        "dms_end": 61.92,
+        "qnums": [],
+        "dms_eng": "We say \"Hola.\"",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 19,
+        "dms_seq": 117274,
+        "dms_speaker": "Sam",
+        "dms_start": 62.38,
+        "dms_end": 63.66,
+        "qnums": [],
+        "dms_eng": "Hola, Maria!",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 20,
+        "dms_seq": 117275,
+        "dms_speaker": "Anna",
+        "dms_start": 63.7,
+        "dms_end": 65.03,
+        "qnums": [],
+        "dms_eng": "Aloha, Maria!",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 21,
+        "dms_seq": 117276,
+        "dms_speaker": "Anna",
+        "dms_start": 65.34,
+        "dms_end": 66.36,
+        "qnums": [],
+        "dms_eng": "I'm Anna.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 22,
+        "dms_seq": 117277,
+        "dms_speaker": "Anna",
+        "dms_start": 66.56,
+        "dms_end": 68.27,
+        "qnums": [2],
+        "dms_eng": "I'm from Hawaii.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 23,
+        "dms_seq": 117278,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117278"
+        }],
+        "dms_speaker": "Anna",
+        "dms_start": 68.4,
+        "dms_end": 71.13,
+        "qnums": [],
+        "dms_eng": "Aloha means \"hi\" in Hawaiian.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 24,
+        "dms_seq": 117279,
+        "dms_speaker": "Maria",
+        "dms_start": 71.66,
+        "dms_end": 73.09,
+        "qnums": [],
+        "dms_eng": "Aloha, Anna.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 25,
+        "dms_seq": 117280,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117280"
+        }],
+        "dms_speaker": "Maria",
+        "dms_start": 73.49,
+        "dms_end": 74.84,
+        "qnums": [],
+        "dms_eng": "Am I saying it right?",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 26,
+        "dms_seq": 117281,
+        "dms_speaker": "Anna",
+        "dms_start": 74.95,
+        "dms_end": 75.86,
+        "qnums": [],
+        "dms_eng": "Perfect!",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 27,
+        "dms_seq": 117282,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117282"
+        }],
+        "dms_speaker": "Ms. Jones",
+        "dms_start": 76.41,
+        "dms_end": 78.94,
+        "qnums": [],
+        "dms_eng": "We have some time before class starts.",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 28,
+        "dms_seq": 117283,
+        "sc_COD": [{
+            "codType": "script",
+            "codSeq": "117283"
+        }],
+        "dms_speaker": "Ms. Jones",
+        "dms_start": 79.1,
+        "dms_end": 81.43,
+        "qnums": [3],
+        "dms_eng": "Why don't you guys show Maria around?",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }, {
+        "seq": 29,
+        "dms_seq": 117284,
+        "dms_speaker": "Sam&Anna",
+        "dms_start": 81.66,
+        "dms_end": 82.8,
+        "qnums": [3],
+        "dms_eng": "Sure!",
+        "dms_kor": {
+            "ko": "",
+            "ja": "",
+            "zh-Hans": "",
+            "zh-Hant": "",
+            "vi": "",
+            "id": "",
+            "es": ""
+        }
+    }]
 };
-
-/*
-start: 0.5,
-end: 21.2,
-*/
